6069be7d2253987cff2e9dd3a352283b463d66bc
alexisrsalazar <121194414+alexisrsalazar@users.noreply.github.com>
2023-03-24 11:47:50 -0600

    Update searchAProductTest.spec.js
    Add comments

--- cypress/e2e/searchAProductTest.spec.js
+++ cypress/e2e/searchAProductTest.spec.js
@@ -8,10 +8,10 @@ const locators = require('../support/pageObject/testData/locators.json');
 describe('Test search a product and print it in the console', () => {
 
     it('Search an Iphone 14 pro max in Amazon', () => {
-        cy.goToAmazon();
-        navigationBar.searchProduct(items.Iphone14ProMax);
-        resultPage.selectAProduct(locators.iphone14ProMax128GBPlata);
-        itemPage.getThePrice();
+        cy.goToAmazon(); //Navigate to Amazon Web
+        navigationBar.searchProduct(items.Iphone14ProMax); //On navigation bar type in the search text box the item Iphone 14 Pro Max and search it
+        resultPage.selectAProduct(locators.iphone14ProMax128GBPlata); //On the result page select the first item in this case an Iphone 14 Pro Max 128GB silver
+        itemPage.getThePrice(); //On the item page get the price and log it in the console panel 
 
     })
     
