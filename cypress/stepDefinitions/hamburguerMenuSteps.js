c308103bda173899d7e6e435302ff74d16cc0ee1
alexisrsalazar <121194414+alexisrsalazar@users.noreply.github.com>
2023-04-04 22:34:37 -0600

    Fixes
    fixes

--- cypress/stepDefinitions/hamburguerMenuSteps.js
+++ cypress/stepDefinitions/hamburguerMenuSteps.js
@@ -1,11 +1,11 @@
 //<reference types="Cypress" />
-import {navigationBar} from "../support/pageObject/pages/navigationBar.js";
-import {resultPage} from "../support/pageObject/pages/resultPage.js";
+import { navigationBar } from "../support/pageObject/pages/navigationBar.js";
+import { resultPage } from "../support/pageObject/pages/resultPage.js";
 import { itemPage } from "../support/pageObject/pages/itemPage.js";
 import { homePage } from "../support/pageObject/pages/homePage.js";
 const items = require('../support/pageObject/testData/products.json');
 const locators = require('../support/pageObject/testData/locators.json');
-import {Given, When, Then} from '@badeball/cypress-cucumber-preprocessor';
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 const deps = require('../fixtures/BuscarPorDepartamento.json')
 const prog = require('../fixtures/ProgramayFuncionalidades.json')
 
@@ -21,11 +21,11 @@ Then("I should see different elements on Buscar Por Categoría and Programas Y C
     cy.get('.hmenu-visible > :nth-child(n+14):nth-child(-n+17) > .hmenu-item')
         .then(($items) => {
             $items.each((index, element) => {
-            cy.wrap(element).should('have.text', deps.departments[index]);
+                cy.wrap(element).should('have.text', deps.departments[index]);
             });
-      });
+        });
 
-      cy.get('.hmenu-visible > :nth-child(n+22):nth-child(-n+25) > .hmenu-item')
+    cy.get('.hmenu-visible > :nth-child(n+22):nth-child(-n+25) > .hmenu-item')
         .each(($items) => {
             listElement.push($items.text().trim());
         })
@@ -35,19 +35,19 @@ Then("I should see different elements on Buscar Por Categoría and Programas Y C
             })
         })
 
-    });
+});
 Then("I should NOT see elements from testing stage", () => {
     cy.get('.hmenu-visible > :nth-child(n+14):nth-child(-n+17) > .hmenu-item')
         .each(($items) => {
-                listElement.push($items.text().trim());
+            listElement.push($items.text().trim());
         })
         .then(() => {
             listElement.forEach((element) => {
                 expect(deps.nonIncludeElements).not.to.include(element)
             })
-            
+
         })
-    })      
+})
 
 
-    
+
